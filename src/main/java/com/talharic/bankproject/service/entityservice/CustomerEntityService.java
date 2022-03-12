1a73e9b36e79758770e0766c73f895b50f20d1d5
Talha Arıç <talharic@gmail.com>
2022-03-13 00:04:17 +0300

    Edit

--- src/main/java/com/talharic/bankproject/service/entityservice/CustomerEntityService.java
+++ src/main/java/com/talharic/bankproject/service/entityservice/CustomerEntityService.java
@@ -1,51 +1,13 @@
 package com.talharic.bankproject.service.entityservice;
 
 import com.talharic.bankproject.entity.Customer;
-import com.talharic.bankproject.enums.CustomerErrorMessage;
-import com.talharic.bankproject.exception.ItemNotFoundException;
 import com.talharic.bankproject.repository.CustomerRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
-
 @Service
-@RequiredArgsConstructor
-public class CustomerEntityService {
+public class CustomerEntityService extends BaseEntityService<Customer, CustomerRepository>{
 
-    private final CustomerRepository customerRepository;
-
-    public List<Customer> findAll(){
-        return customerRepository.findAll();
-    }
-
-    public Optional<Customer> findById(Long id){
-        return customerRepository.findById(id);
-    }
-
-    public Customer save(Customer customer){
-        return customerRepository.save(customer);
-    }
-
-    public void delete(Customer customer){
-        customerRepository.delete(customer);
-    }
-
-    public Customer getByIdWithControl(Long id) {
-
-        Optional<Customer> customerOptional = findById(id);
-
-        Customer customer;
-        if(customerOptional.isPresent()){
-            customer = customerOptional.get();
-        }else{
-            throw new ItemNotFoundException(CustomerErrorMessage.CUSTOMER_ERROR_MESSAGE);
-        }
-        return customer;
-    }
-
-    public boolean existById(Long id){
-        return customerRepository.existsById(id);
+    public CustomerEntityService(CustomerRepository repository) {
+        super(repository);
     }
 }
