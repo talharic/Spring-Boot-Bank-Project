b4787321d8d680fa79639b23234c380c6f63e18f
Talha Arıç <talharic@gmail.com>
2022-03-12 12:09:59 +0300

    Add exception

---
+++ src/main/java/com/talharic/bankproject/exception/ExceptionResponse.java
@@ -0,0 +1,15 @@
+package com.talharic.bankproject.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class ExceptionResponse {
+
+    private Date errorDate;
+    private String message;
+    private String detail;
+}
